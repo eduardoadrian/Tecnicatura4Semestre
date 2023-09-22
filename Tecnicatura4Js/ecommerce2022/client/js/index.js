bb9041d5427ee6ab11dca75274de93225fef2b53
eduardoadrian <adrianeduardopalma2@gmail.com>
2023-09-22 06:39:19 -0300

    Clase 02 E-commerce Basico Parte 1 ,2

--- Tecnicatura4Js/ecommerce2022/client/js/index.js
+++ Tecnicatura4Js/ecommerce2022/client/js/index.js
@@ -1,11 +1,31 @@
 const shopContent = document.getElementById("shopContent");
+const cart = [];//Este es nuestro carrito, un array vacio
 
 productos.forEach((product) =>{
     const content = document.createElement("div");
+    content.className = "card";
     content.innerHTML = `
     <img src="${product.img}">
     <h3>${product.productName}</h3>
-    <p>${product.price} $</p>
+    <p> class="price">${product.price} $</p>
     `;
     shopContent.append(content);
+
+    //boton comprar
+    const buyButton = document.createElement("button");
+    buyButton.innerText = "Buy";
+
+    content.append(buyButton);
+
+    //evento de escucha a travez de un click
+    buyButton.addEventListener("click", () => {
+        cart.push({ //cargamos en el carrito
+            id: product.id,
+            productName: product.productName,
+            price: product.price,
+            quanty: product.quanty,
+            img: product.img,
+        });
+        console.log(cart)
+    })
 });
