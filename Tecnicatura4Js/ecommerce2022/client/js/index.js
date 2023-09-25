d140c3d8771f07c80b211bff831f1bbe40b63301
eduardoadrian <adrianeduardopalma2@gmail.com>
2023-09-25 05:58:42 -0300

    Clase 03 E-commerce Básico Parte 03

--- Tecnicatura4Js/ecommerce2022/client/js/index.js
+++ Tecnicatura4Js/ecommerce2022/client/js/index.js
@@ -19,13 +19,23 @@ productos.forEach((product) =>{
 
     //evento de escucha a travez de un click
     buyButton.addEventListener("click", () => {
-        cart.push({ //cargamos en el carrito
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: product.quanty,
-            img: product.img,
-        });
-        console.log(cart)
-    })
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+        if(repeat) {
+            cart.map((prod)=> {
+                if(prod.id === product.id) {
+                    prod.quanty++;
+                }
+            });
+        }else {
+            cart.push({ //cargamos en el carrito
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+            });
+        }
+
+    });
 });
