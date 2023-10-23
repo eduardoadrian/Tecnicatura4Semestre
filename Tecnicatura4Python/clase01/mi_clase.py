662e0dda7261efba9ac15be9c377f6480d1bbc49
eduardoadrian <adrianeduardopalma2@gmail.com>
2023-10-23 02:00:46 -0300

    Clase 05 Profundizando en String

---
+++ Tecnicatura4Python/clase01/mi_clase.py
@@ -0,0 +1,24 @@
+
+
+class MiClase:
+    '''
+    Este es un ejemplo de la documentacion
+    de esta clase.
+    Es solo para saber que aqui esta MiClase
+    '''
+
+    def __init__(self):
+        '''
+        Aqui esta el método dunder init
+        Este método es el de inicio
+        para esta clase
+        '''
+
+    def mi_metodo(self, param1, param2):
+        '''
+        Este es el método creado por mi que recibe parámetros
+        Detectó los parámetros automaticamente
+        :param param1: Este es el parámetro número 1
+        :param param2: Este es el parámetro número 2
+        :return: Este es el valor de retorno
+        '''
